85e37ab6c82101cbc78375c173d59305b0b1d9a1
Angela Popa <angela.popa@student.uibk.ac.at>
2020-06-22 22:20:43 +0200

    plotting rating frequency per country

--- src/at/uibk/epc/frequency/epc_frequency_per_country.py
+++ src/at/uibk/epc/frequency/epc_frequency_per_country.py
@@ -1,57 +1,81 @@
+
 from pymongo import MongoClient
-from pprint import pprint  # pretty print
-import pandas as pd
-from pandas import DataFrame
-import matplotlib.pyplot as plt
-import numpy as np
-import os
+from frequency import epcFrequencyForCountry
 
-# connect to MongoDB
-client = MongoClient(
-    'mongodb+srv://epcuser3:pw13epc@cluster0-aivtv.mongodb.net/test?retryWrites=true&w=majority')
-# database
-db = client['EPC']
-# collection
-collection = db.get_collection('EPC_Scottland')
 
-# preparing figure name
-country = 'Scottland'
-here = os.path.dirname(os.path.abspath(__file__))
-filename = os.path.join(here, country + '_EPC_Plot.png')
+def getRawDataScottland(country):
+    print(country)
+    scottlandConnectionString = 'mongodb+srv://epcuser3:pw13epc@cluster0-aivtv.mongodb.net/test?retryWrites=true&w=majority'
+    clientScottland = MongoClient(scottlandConnectionString)
+    db = clientScottland['EPC']
+    collectionScottland = db.get_collection('EPC_' + country)
 
-pprint(collection.full_name)
-# count documents with no filter
-pprint(str(collection.count_documents({})) + ' documents aka buildings')
-# one json document
-# pprint(col.find_one())
+    print(collectionScottland.full_name)
+    print(str(collectionScottland.count_documents({})) +
+          ' documents aka buildings')
+    # extract only the rating level
+    dbData = collectionScottland.find({},
+                                      {'awardedRating.ratingLevel': 1, '_id': 0})
+    MongoClient.close
+    return dbData
 
-# extract only the needed data
-dbData = collection.find({},
-                         {'awardedRating.ratingLevel': 1, '_id': 0})
 
-print('Extracting data ...')
+def getRawDataIreland(country):
+    irelandConnectionString = 'mongodb+srv://epcuser2:pw12epc559@epcfull-2jvr7.mongodb.net/test?retryWrites=true&w=majority'
+    clientIreland = MongoClient(irelandConnectionString)
+    db = clientIreland['EPC_Full']
+    collectionIreland = db.get_collection('EPC')
 
-# convert it into a list
-dataFrameList = list()
-for d in dbData:
-    dataFrameList.append(d['awardedRating']['ratingLevel'])
+    print(collectionIreland.full_name)
+    print(str(collectionIreland.count_documents({})) +
+          ' documents aka buildings')
+    # extract only the rating level
+    dbData = collectionIreland.find({},
+                                    {'awardedRating.ratingLevel': 1, '_id': 0})
+    MongoClient.close
+    return dbData
 
-# load list into a panda dataframe
-index = pd.Index(dataFrameList, name="Ratings")
-print(index.value_counts())
 
-# sort by rating levels (label) => sort_index
-pd.DataFrame(index.value_counts()).sort_index(axis=0, ascending=True).plot(kind='bar', color='green',
-                                                                           alpha=0.75, rot=0, legend=False)
-plt.title(country)
-plt.xlabel('Energy Performance Ratings')
-plt.ylabel('Number of buildings')
-plt.ylim((0, 300000))
-plt.savefig(filename, bbox_inches="tight")
+def getRawDataFrance(country):
+    connectionString = 'mongodb+srv://epcuser5:pw15epc@cluster0-929id.mongodb.net/test?retryWrites=true&w=majority'
+    client = MongoClient(connectionString)
+    db = client['EPC']
+    collection = db.get_collection('EPC_' + country)
 
-# sort by number of occurences  => sort_values
-# index.value_counts().sort_values(axis=0, ascending=True).plot(kind='bar', color='green',
-#                                                       alpha=0.75, rot=0)
+    print(collection.full_name)
+    print(str(collection.count_documents({})) +
+          ' documents aka buildings')
+    # extract only the rating level
+    dbData = collection.find({},
+                             {'awardedRating.ratingLevel': 1, '_id': 0})
+    MongoClient.close
+    return dbData
 
-#bins = np.unique(np.array(dataFrameList))
-#print("number of bins " + str(len(bins)))
+
+def getRawDataEngland(country):
+    connectionString = 'mongodb+srv://epcuser4:pw14epc@cluster0-1w0r9.mongodb.net/test?retryWrites=true&w=majority'
+    client = MongoClient(connectionString)
+    db = client['EPC']
+    collection = db.get_collection('EPC_' + country)
+
+    print(collection.full_name)
+    print(str(collection.count_documents({})) +
+          ' documents aka buildings')
+    # extract only the rating level
+    dbData = collection.find({},
+                             {'awardedRating.ratingLevel': 1, '_id': 0})
+    MongoClient.close
+    return dbData
+
+
+scottland = 'Scottland'
+epcFrequencyForCountry(getRawDataScottland(scottland), scottland)
+
+ireland = 'Ireland'
+epcFrequencyForCountry(getRawDataIreland(ireland), ireland)
+
+france = 'France'
+epcFrequencyForCountry(getRawDataFrance(france), france)
+
+england = 'England'
+epcFrequencyForCountry(getRawDataEngland(england), england)
