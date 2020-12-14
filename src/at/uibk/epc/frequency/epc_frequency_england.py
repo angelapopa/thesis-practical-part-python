f955ca28285fbbfa15135fa91398d187e0737f72
Angela Popa <angela.popa@student.uibk.ac.at>
2020-12-14 15:46:57 +0100

    switching to nonadmin user

--- src/at/uibk/epc/frequency/epc_frequency_england.py
+++ src/at/uibk/epc/frequency/epc_frequency_england.py
@@ -4,7 +4,7 @@ from frequency import getRawData
 from frequency import epcFrequencyForCountry
 
 #connectionString = 'mongodb+srv://epcuser4:pw14epc@cluster0-1w0r9.mongodb.net/test?retryWrites=true&w=majority'
-connectionString = 'mongodb+srv://admin:admin@cluster0.ojmf2.mongodb.net/EPC?retryWrites=true&w=majority'
+connectionString = 'mongodb+srv://engl_1:GY9s0BNDrTNjvLFK@cluster0.ojmf2.mongodb.net/EPC?retryWrites=true&w=majority'
 
 queryLimit = 100000
 Xlimit = 45000
