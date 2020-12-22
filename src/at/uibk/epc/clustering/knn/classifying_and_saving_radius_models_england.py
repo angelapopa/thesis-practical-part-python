a9ddcfbdc897145f459719dbcca48eadc8c7d7e4
Angela Popa <angela.popa@student.uibk.ac.at>
2020-12-22 19:35:24 +0100

    renaming knn file from clustering to classifying

---
+++ src/at/uibk/epc/clustering/knn/classifying_and_saving_radius_models_england.py
@@ -0,0 +1,16 @@
+from classifying_models.db_data_per_country import getRawDataFromDB
+from classifying_models.classifying_and_saving_radius_models import classifyingAndSavingRadiusModels
+
+#englandConnectionString = 'mongodb+srv://epcuser4:pw14epc@cluster0-1w0r9.mongodb.net/test?retryWrites=true&w=majority'
+englandConnectionString = 'mongodb+srv://engl_1:GY9s0BNDrTNjvLFK@cluster0.ojmf2.mongodb.net/EPC?retryWrites=true&w=majority'
+queryThermalDataFields = 'ratedDwelling.thermalData.finalEnergyDemand.value'
+englandCountryString = "England"
+# 759464
+#englandQueryLimit = 105000
+englandQueryLimit = 100000
+
+dbData = getRawDataFromDB(
+    englandCountryString, englandConnectionString, queryThermalDataFields, englandQueryLimit)
+
+classifyingAndSavingRadiusModels(
+    englandCountryString.lower(), queryThermalDataFields, dbData)
